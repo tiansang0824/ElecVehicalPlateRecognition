44ec37ebb65789ff73dac08824b44f6577ef644c
tiansang <zhanghaotian0824@qq.com>
2024-02-26 17:06:17 +0800

    重写了Matcher，现在这个类可以通过single2all()函数，实现对单个字符的字符识别功能。

--- src/Match.py
+++ src/Match.py
@@ -1,10 +1,7 @@
-"""
-用于模板匹配
-"""
-
+import os
 import numpy as np
 import cv2
-import os
+import matplotlib.pyplot as plt
 
 
 def read_dir(dir_path):
@@ -21,65 +18,74 @@ def read_dir(dir_path):
 
 
 class Matcher:
-    templ_img_path = './refer'  # 保存模板图片路径
     templates = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-                 'W',
-                 'X', 'Y', 'Z']  # 模板图片索引
-    templ_img_list = list()  # 模板图片路径列表
-    char_img_path = ''  # 保存字符图片路径
-    char_img_path_list = []  # 保存每个单字符图片路径
-    char_list = list()  # 保存识别后的字符
+                 'W', 'X', 'Y', 'Z']
+    templ_path = '../refer/'  # 模板图片路径
+    char_img_path = []  # 字符图片路径
 
-    def __init__(self, char_img_path):
+    def __init__(self, path):
         """
-        构造函数，获取字符图片总路径以及获取每个单字符的路径
-        :param char_img_path:
+        构造函数
+        :param path: 字符图片路径
         """
-        self.char_img_path = char_img_path  # 获取字符图片总路径
-        """获取单字符路径"""
-        # self.char_img_path_list = read_dir(self.char_img_path)
+        self.char_img_path_list = read_dir(path)
 
-    def load_templ_img_list(self):
-        """用于加载模板图片路径列表"""
-        self.templ_img_list = []
+    def single2all(self, char_image_path, template_path):
+        """
+        一个字符图片匹配全部模板。
+        :param char_image_path:
+        :param template_path:
+        :return:
+        """
+        """ 读取和处理字符图片 """
+        img = cv2.imdecode(np.fromfile(char_image_path, dtype=np.uint8), 1)  # 读取图片
+        img = cv2.GaussianBlur(img, (3, 3), 0)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        char_img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        """ 读取全部模板图片路径 """
+        templ_list = list()
         for i in range(len(self.templates)):
-            templ_img_set = read_dir(self.templ_img_path + '/' + self.templates[i])  # 合成单字符路径
-            self.templ_img_list.append(templ_img_set)  # 添加到templ_img_list中。
-        print(f'self.templ_img_list: {self.templ_img_list}')
+            templ_set = read_dir(template_path + self.templates[i])
+            templ_list.append(templ_set)
+        # print(templ_list)
+        """ 遍历模板，进行模板匹配 """
+        best_scores = list()
+        for templ_set in templ_list:
+            scores = list()
+            for single_templ_img_path in templ_set:
+                """ 读取和处理模板图片 """
+                templ = cv2.imdecode(np.fromfile(single_templ_img_path, dtype=np.uint8), 1)
+                templ = cv2.GaussianBlur(templ, (3, 3), 0)
+                templ = cv2.cvtColor(templ, cv2.COLOR_BGR2GRAY)
+                templ = cv2.threshold(templ, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+                """ 修改字符图片的尺寸 """
+                height, width = templ.shape
+                char_img = cv2.resize(char_img, (width, height))
+                """ 模板匹配 """
+                result = cv2.matchTemplate(char_img, templ, cv2.TM_CCOEFF)[0][0]
+                scores.append(result)
+            print(f'max score: {max(scores): 13} in {scores}')
+            best_scores.append(max(scores))
+        print(f'>> best_score的内容为: {best_scores}')
+        print(f'>> best_scores的最大值为: {max(best_scores)}, 对应的下标是：{best_scores.index(max(best_scores))}')
+        print(f'>> 待确认字符为: {self.templates[best_scores.index(max(best_scores))]}')
 
-    def single2batch(self, img_path):
+    def match_all(self):
         """
-        单个字符匹配所有模板
-        :param img_path: 单字符路径
-        :return: 返回匹配结果（字符）
+        该函数用于一次性匹配所有字符。
+
+        有了single2all以后，这个函数的实现就简单很多了
+        1. 找到所有字符图片的路径
+        2. 遍历每个路径，对每个路径对应的字符图片应用single2all()函数。
+        3. 收集每次遍历的识别结果。
+
+        :return: 返回一个列表，该列表中的每一个元素对应一个识别出来的字符。
         """
-        """处理待匹配字符"""
-        char_img = cv2.imdecode(np.fromfile(img_path, dtype=np.uint8), 1)  # 获取图片
-        char_img = cv2.GaussianBlur(char_img, (3, 3), 0)  # 高斯去噪
-        char_img = cv2.cvtColor(char_img, cv2.COLOR_BGR2GRAY)  # 灰度化
-        char_img = cv2.threshold(char_img, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]  # 二值化
-        """获取模板图片路径列表（已经移动到self.load_templ_img_list）"""
-        """ 遍历模板图片，与字符图片进行对比 """
-        best_score = []
-        for templ_img_set in self.templ_img_list:
-            score = list()
-            for templ_img_path in templ_img_set:
-                templ_img = cv2.imdecode(np.fromfile(templ_img_path, dtype=np.uint8), 1)
-                # templ_img = cv2.GaussianBlur(templ_img, (3, 3), 0)
-                templ_img = cv2.cvtColor(templ_img, cv2.COLOR_BGR2GRAY)
-                templ_img = cv2.threshold(templ_img, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-                result = cv2.matchTemplate(char_img, templ_img, cv2.TM_CCOEFF)
-                score.append(result[0][0])
-            best_score.append(max(score))
-        print(f'best_score: {best_score}')
-        matched_char = self.templates[best_score.index((max(best_score)))]
-        return matched_char
+        read_dir(char_img_path)
 
 
 if __name__ == '__main__':
     m = Matcher('./divide/test12')
-    print(m.char_img_path_list)
-    m.load_templ_img_list()
-    c = m.single2batch('./divide/test12/test12-0.jpg')
-    print(f'c: {c}')
+    print(m.char_img_path)
+    m.single2all('./divide/test12/test12-4.jpg', './refer/')
--- src/demo_collection/04_demo_add_templ.py
+++ src/demo_collection/04_demo_add_templ.py
@@ -2,14 +2,13 @@ import numpy as np
 import cv2
 import os
 
-char_img_path = '../divide/test12/test12-0.jpg'
-char_signal = 'M'
+char_img_path = '../divide/test12/test12-5.jpg'
 
 img_width = 20
 img_height = 20
 
 char_img = cv2.imdecode(np.fromfile(char_img_path, dtype=np.uint8), 1)
-char_img = cv2.GaussianBlur(char_img, (3,3),0)
+char_img = cv2.GaussianBlur(char_img, (3, 3), 0)
 char_img = cv2.cvtColor(char_img, cv2.COLOR_BGR2GRAY)
 char_img = cv2.threshold(char_img, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
@@ -18,4 +17,4 @@ char_img = cv2.resize(char_img, (img_width, img_height))
 cv2.imshow('char_img', char_img)
 cv2.waitKey(0)
 
-cv2.imwrite('../refer/M/demo1001.jpg', char_img)
+cv2.imwrite('../refer/9/demo1001.jpg', char_img)
