1f1f93d118978e5d349ee78ffa4d2e723e632193
tiansang <zhanghaotian0824@qq.com>
2024-02-26 17:32:03 +0800

    Matcher基本完成，接下来的工作就是完善各个类的接口了。

--- src/Match.py
+++ src/Match.py
@@ -21,8 +21,9 @@ class Matcher:
     templates = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
                  'W', 'X', 'Y', 'Z']
-    templ_path = '../refer/'  # 模板图片路径
-    char_img_path = []  # 字符图片路径
+    templ_path = './refer/'  # 模板图片路径
+    char_img_path_list = ''  # 字符图片路径
+    answer = ''
 
     def __init__(self, path):
         """
@@ -65,11 +66,12 @@ class Matcher:
                 """ 模板匹配 """
                 result = cv2.matchTemplate(char_img, templ, cv2.TM_CCOEFF)[0][0]
                 scores.append(result)
-            print(f'max score: {max(scores): 13} in {scores}')
+            print(f'max score: {max(scores): 13.2f} in {scores}')
             best_scores.append(max(scores))
         print(f'>> best_score的内容为: {best_scores}')
         print(f'>> best_scores的最大值为: {max(best_scores)}, 对应的下标是：{best_scores.index(max(best_scores))}')
         print(f'>> 待确认字符为: {self.templates[best_scores.index(max(best_scores))]}')
+        return self.templates[best_scores.index(max(best_scores))]
 
     def match_all(self):
         """
@@ -82,10 +84,22 @@ class Matcher:
 
         :return: 返回一个列表，该列表中的每一个元素对应一个识别出来的字符。
         """
-        read_dir(char_img_path)
+        self.answer = list()
+        char_img_set = self.char_img_path_list[0:len(self.char_img_path_list) - 1]  # char_img_set 用于保存单字符字符图片
+        # print(f'char_img_set: {char_img_set}')
+        for char_img_path in char_img_set:
+            character = self.single2all(char_img_path, self.templ_path)  # 对单个字符进行比较
+            self.answer.append(character)
+        print(f'>> answer is : {self.answer}')
+
+    def get_final_answer(self):
+        return ''.join(self.answer)
 
 
 if __name__ == '__main__':
     m = Matcher('./divide/test12')
-    print(m.char_img_path)
-    m.single2all('./divide/test12/test12-4.jpg', './refer/')
+    print(m.char_img_path_list)
+    # m.single2all('./divide/test12/test12-4.jpg', './refer/')
+    m.match_all()
+    final_answer = m.get_final_answer()
+    print(f'final answer is : {final_answer}')
